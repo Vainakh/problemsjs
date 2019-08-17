629906f91ba3d45b8a818444fc6acc59445dcaf0
Adlan <adlanyandarbiev@gmail.com>
2019-08-17 14:35:12 -0700

    Finish printEvens.js

---
+++ printEvens.js
@@ -0,0 +1,12 @@
+const array = [[1, 2, 3], [4, 5, 6], [7, 8], [9, 10, 11, 12]];
+
+// function printEvens(array){
+//     for (let i = 0; i < array.length; i += 1){
+//         for (let j = 0; j < array[i].length; j += 1){
+//             const currentEl = array[i][j];
+//             if (currentEl % 2 === 0){
+//              console.log(currentEl);
+//             }
+//         }
+//     }
+// }
