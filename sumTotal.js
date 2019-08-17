8c3a487f23a5dd8e71f3b282cf9893dc9241c74f
Adlan <adlanyandarbiev@gmail.com>
2019-08-17 14:56:22 -0700

    Finish sumTotal

--- sumTotal.js
+++ sumTotal.js
@@ -1,1 +1,16 @@
 const nestedArr = [[1, 2], [3, 4], [5, 6]];
+
+function sumTotal(array){
+    let sumOfNumbers = 0;
+
+    for (let i = 0; i < array.length; i += 1) {
+        for (let j = 0; j < array[i].length; j += 1) {
+
+            sumOfNumbers += array[i][j];
+    }
+}
+    return sumOfNumbers;
+
+}
+
+console.log(sumTotal(nestedArr));
