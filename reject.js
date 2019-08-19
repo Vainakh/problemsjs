98f5dfab546e6057113c2d0eac2ed686868677d0
Adlan <adlanyandarbiev@gmail.com>
2019-08-18 20:24:04 -0700

    Add test case

--- reject.js
+++ reject.js
@@ -13,5 +13,10 @@ function reject(array, cb){
         return newArr;
     }
 
+    console.log(reject([1, 2, 3, 4], function (val) {
+        return val > 2;
+    })); // [1,2]
 
-    
+    console.log(reject([2, 3, 4, 5], function (val) {
+        return val % 2 === 0;
+    })); // [3,5]
