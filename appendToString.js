0ba80e600d6c4ae3181b59e63fff6f9a801898f2
Adlan <adlanyandarbiev@gmail.com>
2019-08-20 10:12:52 -0700

    Add test case

--- appendToString.js
+++ appendToString.js
@@ -1,3 +1,8 @@
 function appendToString(string1, string2) {
   return (string1 + string2);
 };
+console.log(appendToString("Hello", " World!")); // "Hello World!"
+appendToString("Foo", "bar"); // "Foobar"
+appendToString("bar", "Foo"); // "barFoo"
+appendToString("", "test"); // "test"
+appendToString("other test", ""); // "other test"
