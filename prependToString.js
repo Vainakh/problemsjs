872f4a7b593bec04c0c74740cefeb1fad6b15176
Adlan <adlanyandarbiev@gmail.com>
2019-08-20 10:14:54 -0700

    Add test case

--- prependToString.js
+++ prependToString.js
@@ -1,3 +1,7 @@
 function prependToString(string1, string2){
   return string2 + string1
 }
+
+prependToString('awesome', 'very') // 'veryawesome'
+prependToString('world', 'hello ') // 'hello world'
+prependToString('nothing', '') // 'nothing'
