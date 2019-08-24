7ef4037ce8045a5d119888091de8794b963d4445
Adlan <adlanyandarbiev@gmail.com>
2019-08-24 16:40:32 -0700

    Add test case

--- indexOf.js
+++ indexOf.js
@@ -6,3 +6,12 @@ function indexOf(array, number){
   }
   return -1
 }
+
+var arr = [5, 10, 15, 20];
+indexOf(arr, 20); // 3
+
+var arr2 = [1, 2, 3, 4, 5];
+indexOf(arr2, 2); // 1
+
+var arr3 = [1, 2];
+indexOf(arr3, 10); // -1
