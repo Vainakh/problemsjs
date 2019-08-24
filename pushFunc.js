8bc7683ad24aab9773558a516f07dce32d2dcb49
Adlan <adlanyandarbiev@gmail.com>
2019-08-24 16:44:46 -0700

    Finish function

---
+++ pushFunc.js
@@ -0,0 +1,15 @@
+function push(array, number){
+  array[array.length] = number;
+  array;
+  return array.length;
+};
+
+var arr = [1, 2, 3];
+push(arr, 10); // 4
+
+arr; // [1, 2, 3, 10]
+
+var secondArr = [5, 2, 1, 6];
+push(secondArr, 20); // 5
+
+arr; // [5, 2, 1, 6, 20]
