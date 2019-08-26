e6a37550897c0d1201afe0c7be9e4801683f7d77
Adlan <adlanyandarbiev@gmail.com>
2019-08-26 06:22:44 -0700

    Add test cases

--- popFunc.js
+++ popFunc.js
@@ -1,3 +1,11 @@
 function pop(arr) {
  return arr.splice(-1, 1)[0];
 }
+
+var arr = [1, 2, 3, 4];
+pop(arr); // 4
+// arr; // [1, 2, 3]
+
+// var emptyArr = [];
+// pop(emptyArr); // undefined
+// emptyArr.length; // 0
