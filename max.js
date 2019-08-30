4f9a25abe3b6fa958472402ad25432798f6fee83
Adlan <adlanyandarbiev@gmail.com>
2019-08-30 16:02:05 -0700

    Finish function

--- max.js
+++ max.js
@@ -1,3 +1,13 @@
 Write a function called max, which accepts an array and returns the highest value.
 
 Do not use the built-in Math.max() function!
+
+function max(arr) {
+  return arr.reduce(function (x, y) {
+    return ( x > y ? x : y );
+  });
+}
+
+max([5, 1, 4, 7, 1, 2]); // 7
+max([3, 4, 12, 1, 8]); // 12
+max([-1, 6, 3, 2.2, -10, -4]); // 6
