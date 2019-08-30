439ccfbc3aed6f902c4f5cef4e25693baaff770a
Adlan <adlanyandarbiev@gmail.com>
2019-08-30 15:59:19 -0700

    Finish function

--- _27_reverse.js
+++ _27_reverse.js
@@ -3,3 +3,15 @@ Write a function called reverse, which accepts an array and returns the same arr
 Note: returning the same array is called an in-place operation, since no additional space is used. https://en.wikipedia.org/wiki/In-place_algorithm
 
 Do not use the built in Array.reverse() function!
+
+function reverse(array) {
+  if(array.length === 0){
+    return [];
+  }
+  for (var i = 0; i <= (array.length / 2); i++) {
+    let currentElement = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = currentElement;
+  }
+  return array;
+}
