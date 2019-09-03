c3c9bc627d74fbd0bf10361770c6dc809496c6da
Adlan <adlanyandarbiev@gmail.com>
2019-09-03 05:30:52 -0700

    Add problem description

--- _02_callBackEach.js
+++ _02_callBackEach.js
@@ -1,3 +1,7 @@
+// Write a function called prependToString, which accepts two strings.
+//
+// The function should return a new string with the second string
+// prepended to the first string.
 function each(array, cb){
     for (let i = 0; i < array.length; i += 1){
         let currentElement = array[i];
