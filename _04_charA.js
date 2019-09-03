dda21c83997a6ba035a11d482af45da2def82189
Adlan <adlanyandarbiev@gmail.com>
2019-09-03 05:32:03 -0700

    Add problem description

--- _04_charA.js
+++ _04_charA.js
@@ -1,3 +1,9 @@
+Write a function called charAt which accepts a string and an index (number) and returns the character at that index.
+
+The function should return an empty string if the number is greater than the length of the string.
+
+Do not use the built in charAt method - the tests will fail if you do! 
+
 function charAt(string, index){
 
     if (string.length <= index){
