227ef9eb92f82c668011fa36ef20ad54867b341a
Adlan <adlanyandarbiev@gmail.com>
2019-09-03 10:29:53 -0700

    Add description

--- _08_lastIndexOf.js
+++ _08_lastIndexOf.js
@@ -1,3 +1,10 @@
+Write a function called stringLastIndexOf, which accepts two strings: the first is a word and the second is a single character.
+
+The function should return the last index at which the character exists or -1 if the character is not found.
+
+Do not use the built in String.lastIndexOf() function!
+
+
 function lastIndexOf(array, number){
   for (let i = array.length - 1; i > 0; i -= 1){
     if(array[i] === number){
