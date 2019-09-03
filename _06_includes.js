a7ec2c0dde2d52b2e57a4631a2f06a88e18ce6d9
Adlan <adlanyandarbiev@gmail.com>
2019-09-03 10:25:40 -0700

    Add problem description

--- _06_includes.js
+++ _06_includes.js
@@ -1,3 +1,9 @@
+Write a function called stringIncludes, which accepts two strings: the first string is a word and the second string is a single character.
+
+The function should return true if the first string includes the character, otherwise it should return false.
+
+Do not use the built in String.includes() function!
+
 function includes(array, number){
   for (let i = 0; i < array.length; i += 1){
     if(array[i] === number){
