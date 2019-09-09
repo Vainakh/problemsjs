16b5c812d3844a8db6bb2df9d280dbb700d74070
Adlan <adlanyandarbiev@gmail.com>
2019-09-09 13:51:00 -0700

    Add function body

--- _33_values.js
+++ _33_values.js
@@ -2,3 +2,12 @@
 Write a function called values, which accepts an object and returns an array of all of the values in the object.
 
 Do not use the built in Object.values() function!
+
+function values(obj){
+  let newArray = [];
+
+  for (var key in obj){
+    newArray.push((obj[key]));
+  }
+  return newArray;
+}
