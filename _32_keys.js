039ef0c22dac945dcc94fbe2e99faef36fec0a48
Adlan <adlanyandarbiev@gmail.com>
2019-09-09 13:47:42 -0700

    Add function body

--- _32_keys.js
+++ _32_keys.js
@@ -1,3 +1,12 @@
 Write a function called keys, which accepts an object and returns an array of all of the keys in the object.
 
 Do not use the built in Object.keys() function!
+
+function keys(obj){
+  let newArray = [];
+
+  for (var key in obj){
+    newArray.push(key);
+  }
+  return newArray;
+}
