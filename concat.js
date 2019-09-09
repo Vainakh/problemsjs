2840721b6454cc1d14429634fd4d9ffbce768e9a
Adlan <adlanyandarbiev@gmail.com>
2019-09-08 19:55:11 -0700

    Add problem description

---
+++ concat.js
@@ -0,0 +1,5 @@
+Write a function called concat, which accepts an array, and one, two, or three additional arguments (all of which are arrays).
+
+The function returns a new array with all of the remaining parameters joined with the array.
+
+Do not use the built in Array.concat() function!
