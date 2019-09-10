b56142ead28a45c26f0fe945e4fbbe4336874fb3
Adlan <adlanyandarbiev@gmail.com>
2019-09-10 14:44:09 -0700

    Add function body

--- _34_swapKeysAndValues.js
+++ _34_swapKeysAndValues.js
@@ -1,3 +1,17 @@
 Write a function called swapKeyAndValue, which accepts an object and a key.
 
 The function should return a new object with the given key and its value flipped, and all the other key/value pairs unchanged.
+
+function swapKeyAndValue(obj, key){
+  let newObj = {};
+  let newArray = Object.keys(obj);
+
+  for (let i = 0; i < newArray.length; i += 1){
+    if(newArray[i] === key){
+      newObj[obj[key]] = key;
+    } else {
+      newObj[newArray[i]] = obj[newArray[i]]
+    }
+  }
+  return newObj;
+}
