156dd1500c14f5b6ee6e6d21763af005bfc09d80
Adlan <adlanyandarbiev@gmail.com>
2019-09-10 14:43:35 -0700

    Add function body

---
+++ _34_swapKeysAndValues.js
@@ -0,0 +1,3 @@
+Write a function called swapKeyAndValue, which accepts an object and a key.
+
+The function should return a new object with the given key and its value flipped, and all the other key/value pairs unchanged.
