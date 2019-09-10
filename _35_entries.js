25bcc7920d63edddc72262a9789d90390324a2fd
Adlan <adlanyandarbiev@gmail.com>
2019-09-10 16:25:40 -0700

    Add function body

--- _35_entries.js
+++ _35_entries.js
@@ -3,3 +3,12 @@ Write a function called entries, which accepts an object and returns an array of
 In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element is the value.
 
 Do not use the built in Object.entries() function!
+
+function entries(obj){
+  let newArray = [];
+
+  for (var key in obj){
+    newArray.push([key, obj[key]])
+  }
+  return newArray;
+}
