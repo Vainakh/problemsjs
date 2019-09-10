ebcdd3e79c93ebb2a7b0f3ff8c1848ffda10ae76
Adlan <adlanyandarbiev@gmail.com>
2019-09-10 16:24:45 -0700

    Add function description

---
+++ _35_entries.js
@@ -0,0 +1,5 @@
+Write a function called entries, which accepts an object and returns an array of arrays of key-value pairs.
+
+In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element is the value.
+
+Do not use the built in Object.entries() function!
