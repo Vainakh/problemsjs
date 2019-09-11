40eb51ac21d573c6dfcc3b4949bbbcb195d8ac99
Adlan <adlanyandarbiev@gmail.com>
2019-09-11 13:00:51 -0700

    Add problem description

---
+++ _37_some.js
@@ -0,0 +1,5 @@
+Write a function called some which accepts an array and a callback function.
+
+The function should return true if any value in the array passed to the callback returns true, otherwise return false.
+
+Do not use the built in some function, the tests will fail!
