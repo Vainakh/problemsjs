a938b093514f822879e569b6c4f19441d02c34a0
Adlan <adlanyandarbiev@gmail.com>
2019-09-11 12:57:23 -0700

    Add problem description

---
+++ _36_entries.js
@@ -0,0 +1,3 @@
+Write a function called every which accepts an array and a callback function. The function should return true if all values in the array passed to the callback return true, otherwise return false.
+
+Do not use the built in every function, the tests will fail!
