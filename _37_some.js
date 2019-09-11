e488128a0d4e85a7c221a4e7bd5b112fdabd24e2
Adlan <adlanyandarbiev@gmail.com>
2019-09-11 13:01:50 -0700

    Write function body

--- _37_some.js
+++ _37_some.js
@@ -3,3 +3,12 @@ Write a function called some which accepts an array and a callback function.
 The function should return true if any value in the array passed to the callback returns true, otherwise return false.
 
 Do not use the built in some function, the tests will fail!
+
+function some(array, cb){
+  for (let i = 0; i < array.length; i += 1){
+    if(cb(array[i] === true)){
+      return true;
+    }
+  }
+  return false;
+}
