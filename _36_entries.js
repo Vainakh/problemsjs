07836481a453a713e61a1177d5db0b029e9640ab
Adlan <adlanyandarbiev@gmail.com>
2019-09-11 12:58:07 -0700

    Write function

--- _36_entries.js
+++ _36_entries.js
@@ -1,3 +1,12 @@
 Write a function called every which accepts an array and a callback function. The function should return true if all values in the array passed to the callback return true, otherwise return false.
 
 Do not use the built in every function, the tests will fail!
+
+function every(array, cb){
+  for (let i = 0; i < array.length; i += 1){
+    if (cb(array[i]) !== true){
+      return false;
+    }
+  }
+  return true;
+}
