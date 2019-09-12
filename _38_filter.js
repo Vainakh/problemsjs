7e1662ee1ff583abb1297ceba9e8d2235fdfd2e3
Adlan <adlanyandarbiev@gmail.com>
2019-09-12 13:27:22 -0700

    Add problem description

---
+++ _38_filter.js
@@ -0,0 +1,5 @@
+Write a function called filter which accepts an array and a callback function. The callback takes a single parameter called val and return a boolean based on some comparison to the value.
+
+The function should return an array of all values that pass the comparison test.
+
+Do not use the built in filter function, the tests will fail!
