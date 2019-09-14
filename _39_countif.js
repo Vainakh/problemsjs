b7665c8ae3aa4288b7e5a7748f124eff10bc7073
Adlan <adlanyandarbiev@gmail.com>
2019-09-14 14:34:06 -0700

    Add body

--- _39_countif.js
+++ _39_countif.js
@@ -1,3 +1,14 @@
 Write a function called countIf which accepts an array and a callback function. The callback function will return a boolean comparison to its val parameter.
 
 The function should return a count of the number of times the callback returns true when passed each value in the array.
+
+function countIf(array, cb){
+  let count = 0;
+
+  for (let i = 0; i < array.length; i += 1){
+    if(cb(array[i])){
+      count += 1
+    }
+  }
+  return count;
+}
