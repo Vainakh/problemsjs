c82cf4ffa95ef1f550e92fe2e8ad3720ddedc4f8
Adlan <adlanyandarbiev@gmail.com>
2019-09-14 14:33:03 -0700

    Add description

---
+++ _39_countif.js
@@ -0,0 +1,3 @@
+Write a function called countIf which accepts an array and a callback function. The callback function will return a boolean comparison to its val parameter.
+
+The function should return a count of the number of times the callback returns true when passed each value in the array.
