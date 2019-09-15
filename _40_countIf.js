58aa5761e2b51f9fa2fee63d31abbe84ee171a20
Adlan <adlanyandarbiev@gmail.com>
2019-09-15 04:40:11 -0700

    Add function body

--- _40_countIf.js
+++ _40_countIf.js
@@ -1,1 +1,10 @@
-Write a function called countValues which accepts an array and a number and returns the number of times that value appears in the array. 
+Write a function called countValues which accepts an array and a number and returns the number of times that value appears in the array.
+function countValues(array, number){
+  let count = 0;
+  for (let i = 0; i < array.length; i += 1){
+    if(array[i] === number){
+      count += 1;
+    }
+  }
+  return count;
+}
