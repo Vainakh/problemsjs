23b9b1cc9f4606d54fca7b0637931773a46df3a2
Adlan <adlanyandarbiev@gmail.com>
2019-09-17 15:40:50 -0700

    Write body

--- _42_multiples.js
+++ _42_multiples.js
@@ -3,3 +3,11 @@ Implement a function called multiples that accepts two numbers: x and n.
 The function should return the first n multiples of the number x.
 
 Assume that x is a positive integer.
+
+function multiples(x, n) {
+  let multy = [];
+ for (let i = 1; i <= n; i += 1){
+   multy.push(x * i);
+ }
+ return multy;
+}
