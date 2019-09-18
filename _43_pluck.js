a0f47ceef52a68c134cbccbd8787625229f42eaa
Adlan <adlanyandarbiev@gmail.com>
2019-09-18 12:16:56 -0700

    Add fucntion body

--- _43_pluck.js
+++ _43_pluck.js
@@ -1,3 +1,12 @@
 Write a function called pluck, which takes an array of objects and the name of a key.
 
 The function should return an array containing the value associated with that key for each object, or undefined if that key is not present in the object.
+
+function pluck(array, key){
+  let newArray = [];
+  for (let i = 0; i < array.length; i += 1){
+
+    newArray.push(array[i][key]);
+  }
+  return newArray;
+}
