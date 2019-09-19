b535d146b14ba0c8035af2081488cdc54d310eda
Adlan <adlanyandarbiev@gmail.com>
2019-09-19 12:04:21 -0700

    Add test cases

--- _45_minMaxKeyInObject.js
+++ _45_minMaxKeyInObject.js
@@ -17,3 +17,10 @@ function minMaxKeyInObject(obj){
 
   return [lowestKey, highestKey];
 }
+
+
+minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' });
+// [1, 10]
+
+minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
+// [1, 4]
