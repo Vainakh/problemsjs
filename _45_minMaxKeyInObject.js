5e1553b2d6cd67033ee506584f64ccd6ee1d0959
Adlan <adlanyandarbiev@gmail.com>
2019-09-19 12:03:50 -0700

    Finish function body

--- _45_minMaxKeyInObject.js
+++ _45_minMaxKeyInObject.js
@@ -1,3 +1,19 @@
 Write a function called minMaxKeyInObject that accepts an object with numeric keys.*
 
 The function should return an array with the following format: [lowestKey, highestKey]
+
+function minMaxKeyInObject(obj){
+
+  let newArray = [];
+
+  for (let key in obj){
+    newArray.push(key)
+  }
+
+  let sortedArr = newArray.sort(function(a, b){return a-b});
+
+  lowestKey = parseInt(sortedArr[0]);
+  highestKey = parseInt(sortedArr[sortedArr.length - 1]);
+
+  return [lowestKey, highestKey];
+}
