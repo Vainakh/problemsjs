9c4b3c63eadc96c42358d2dd392ee35354fcdd4b
Adlan <adlanyandarbiev@gmail.com>
2019-09-19 11:58:34 -0700

    Finish function body

--- _44_twoHighest.js
+++ _44_twoHighest.js
@@ -5,3 +5,20 @@ The returned value should be an array in the following format: [secondHighest, h
 The order of the numbers passed in could be any order.
 
 Do not use the build in sort() method - the tests will fail!
+
+function twoHighest(array){
+
+	let firstNum = 0;
+	let secondNum = 0;
+
+	for(let i = 0; i < array.length; i += 1){
+		if(firstNum < array[i]){
+			secondNum = firstNum;
+			firstNum = array[i];
+		}else if(secondNum < array[i]){
+			secondNum = array[i];
+		}
+	}
+
+	return [secondNum, firstNum];
+}
