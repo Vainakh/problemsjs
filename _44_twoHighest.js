c248335c3c593839a10864ff26df55cb78f15fa2
Adlan <adlanyandarbiev@gmail.com>
2019-09-19 11:57:43 -0700

    Add description

--- _44_twoHighest.js
+++ _44_twoHighest.js
@@ -1,1 +1,7 @@
+Write a function called twoHighest that takes an array of numbers as its argument and returns the two highest numbers within the array.
 
+The returned value should be an array in the following format: [secondHighest, highest]
+
+The order of the numbers passed in could be any order.
+
+Do not use the build in sort() method - the tests will fail!
