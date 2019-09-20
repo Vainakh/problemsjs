19e6c392a1311c764cb30ac84cf65f12cd25c159
Adlan <adlanyandarbiev@gmail.com>
2019-09-20 07:13:40 -0700

    Add function body

--- _48_countNumbers.js
+++ _48_countNumbers.js
@@ -1,3 +1,14 @@
 Write a function called countNumbers, which accepts an array of strings. The function should return a count of the number of strings in the array that can be successfully converted into a number. For example, the string "1" can be successfully converted to the number 1, but the string "hello" cannot be converted into a number.
 
 Examples:
+
+function countNumbers(array){
+  let count = 0;
+
+  for (let i = 0; i < array.length; i += 1){
+    // if(!(isNaN(array[i])) || array[i] > 0){
+    if(array[i] === '0') count += 1;
+    if(array[i] && parseInt(array[i])) count += 1;
+  }
+  return count;
+}
