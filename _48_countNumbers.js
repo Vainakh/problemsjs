0a414ca9a82e58b5bc17ac2e62343c41cfc4dd56
Adlan <adlanyandarbiev@gmail.com>
2019-09-20 07:14:18 -0700

    Add test cases

--- _48_countNumbers.js
+++ _48_countNumbers.js
@@ -12,3 +12,9 @@ function countNumbers(array){
   }
   return count;
 }
+
+console.log(countNumbers(['a','b','3','awesome','4'])); // 2
+console.log(countNumbers(['32', '55', 'awesome', 'test', '100'])); // 3
+console.log(countNumbers([])); // 0
+console.log(countNumbers(['4','1','0','NaN'])); // 3
+console.log(countNumbers(['7', '12', 'a', '', '6', '8', ' '])); // 4
