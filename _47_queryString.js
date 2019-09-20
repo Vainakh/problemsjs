3b65b2da03af082643816241bfeaf546dcce2ac7
Adlan <adlanyandarbiev@gmail.com>
2019-09-20 07:09:50 -0700

    Numerate and Add description

---
+++ _47_queryString.js
@@ -0,0 +1,3 @@
+A query string, is a way to send data in an HTTP request, you may see it in a URL starting with a question mark. Write a function called toQueryString, which accepts an object and returns a string with each key and value separated by a = and each pair separated by a &. If the value is an array, add another pair to the string with each value
+
+Examples:
