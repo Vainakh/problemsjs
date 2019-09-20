165b4a1ee0e576e50cfdc23944fec6e1a3096419
Adlan <adlanyandarbiev@gmail.com>
2019-09-19 19:29:27 -0700

    Add description

---
+++ _46_stringFromObject.js
@@ -0,0 +1,7 @@
+Write a function called stringFromObject that generates a string from an object's key/value pairs.
+
+The format should be "key = value, key = value".
+
+Each key/value pair should be separated by a comma and space except for the last pair.
+
+Examples:
