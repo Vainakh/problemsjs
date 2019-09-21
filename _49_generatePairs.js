39e6114a1f3eb562cc8edd490f77dc1441fa6e90
Adlan <adlanyandarbiev@gmail.com>
2019-09-21 08:10:20 -0700

    Add description

---
+++ _49_generatePairs.js
@@ -0,0 +1,3 @@
+Write a function generatePairs that accepts an integer and generates an array containing the pairs of integers [a, b]. The pairs should be sorted by increasing values of a then increasing values of b.
+
+Examples:
