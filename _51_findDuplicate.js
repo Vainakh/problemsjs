8f88fae08ad1e6613411b9fe3a246ef20f3ec259
Adlan <adlanyandarbiev@gmail.com>
2019-09-22 15:40:39 -0700

    Addtest cases

--- _51_findDuplicate.js
+++ _51_findDuplicate.js
@@ -12,3 +12,7 @@ function findTheDuplicate(array){
     }
   }
 }
+
+console.log(findTheDuplicate([1,2,1,4,3,12])) // 1
+console.log(findTheDuplicate([6,1,9,5,3,4,9])) // 9
+console.log(findTheDuplicate([2,1,3,4])); // undefined
