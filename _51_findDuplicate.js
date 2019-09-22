272ef8e5296babd6904aeb8993b00bbf2af38b7c
Adlan <adlanyandarbiev@gmail.com>
2019-09-22 15:39:38 -0700

    Add body

---
+++ _51_findDuplicate.js
@@ -0,0 +1,3 @@
+Write a function called findTheDuplicate which accepts an array of numbers containing a single duplicate. The function returns the number which is a duplicate or undefined if there are no duplicates.
+
+Examples:
