d3c763343913e629ad3838832141703a75ccb894
Adlan <adlanyandarbiev@gmail.com>
2019-09-22 15:40:10 -0700

    Finish function

--- _51_findDuplicate.js
+++ _51_findDuplicate.js
@@ -1,3 +1,14 @@
 Write a function called findTheDuplicate which accepts an array of numbers containing a single duplicate. The function returns the number which is a duplicate or undefined if there are no duplicates.
 
 Examples:
+
+function findTheDuplicate(array){
+  let newArray = [];
+  for (let i = 0; i < array.length; i += 1){
+    if(newArray.includes(array[i])){
+      return array[i];
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+}
