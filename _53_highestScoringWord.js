934fd303d9a570235c0dd03960ee74c69bbb7e17
Adlan <adlanyandarbiev@gmail.com>
2019-09-23 04:55:48 -0700

    Add description

---
+++ _53_highestScoringWord.js
@@ -0,0 +1,5 @@
+Given a string of words, your goal is to find the highest scoring word in the string. a is worth 1 point, b is worth 2 points, c is worth 3 points, and so on, all the way up until z, which is worth 26 points. You can assume that strings will consist only of lowercase letters and spaces.
+
+In the event that two words have the same score, return the word that appears first in the string.
+
+Examples:
