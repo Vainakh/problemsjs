2594fbe9bafb725706358e9a866a6d0779372e76
Adlan <adlanyandarbiev@gmail.com>
2019-09-23 04:50:23 -0700

    Add description

---
+++ _52_totalCaps.js
@@ -0,0 +1,3 @@
+Write a function called totalCaps, which accepts an array of strings and returns the total number of capitals in each of the strings. Do not convert the array into a string.
+
+Examples:
