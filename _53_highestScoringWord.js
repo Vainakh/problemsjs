d10b53968eaeaed84091b5a7597ddf9c14059c19
Adlan <adlanyandarbiev@gmail.com>
2019-09-23 04:56:26 -0700

    Add function body

--- _53_highestScoringWord.js
+++ _53_highestScoringWord.js
@@ -3,3 +3,35 @@ Given a string of words, your goal is to find the highest scoring word in the st
 In the event that two words have the same score, return the word that appears first in the string.
 
 Examples:
+
+function highestScoringWord(string){
+  let newString = string.split(" ");
+  let scores = {};
+    for (let i = 0; i < newString.length; i++) {
+      let currentWord = newString[i];
+        scores[currentWord] = wordScore(currentWord);
+        }
+      return Object.keys(scores).sort(function (a, b) {
+    return scores[b] - scores[a];
+  })[0];
+}
+
+function wordScore(word) {
+  let alphabet = " abcdefghijklmnopqrstuvwxyz";
+  let letters = word.split("");
+    let score = 0;
+      letters.forEach(function (letter) {
+      score += alphabet.indexOf(letter);
+    });
+  return score;
+}
+
+
+console.log(highestScoringWord("a b c d e f")); // "f"
+console.log(highestScoringWord("hello world")); // "world"
+console.log(highestScoringWord("go ahead make my day")); // "my"
+console.log(highestScoringWord("there is no place like home")); // "there"
+console.log(highestScoringWord("aaaaaa bbb cc f")); // "aaaaaa"
+console.log(highestScoringWord("bbb cc f aaaaaa")); // "bbb"
+console.log(highestScoringWord("this sentence has two highest scoring words"));
+// "sentence"
