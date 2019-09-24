21facb0765526c4ebb2bb7e52916232004252b86
Adlan <adlanyandarbiev@gmail.com>
2019-09-24 11:30:17 -0700

    Finish test cases

--- _54_separate.js
+++ _54_separate.js
@@ -24,3 +24,26 @@ function separate(array){
   }
   return [...catArray, ...waterArray, ...dogArray];
 }
+
+console.log(separate(['dog','cat','water'])) // ['cat','water','dog']
+
+console.log(separate(['dog','cat','water','cat'])) // ['cat', 'cat', 'water', 'dog'])
+
+console.log(separate(['cat','cat','water','dog','water','cat','water','dog']))
+  // ['cat','cat','cat','water','water','water','dog','dog']
+
+console.log(separate(
+  ['cat','cat','cat','cat','cat',
+    'cat','cat','cat','cat','cat','cat',
+    'cat','cat','cat','cat','cat','cat','cat',
+    'dog','water','water','water','water','water',
+    'water','water','water','water','water','water',
+    'water','water','water'
+]))
+
+// // ['cat','cat','cat','cat','cat',
+//     'cat','cat','cat','cat','cat','cat',
+//     'cat','cat','cat','cat','cat','cat','cat',
+//     'water','water','water','water','water',
+//     'water','water','water','water','water','water',
+//     'water','water','water', 'dog']
