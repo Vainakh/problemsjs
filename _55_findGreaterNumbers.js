3606b3e9b8186a34cc95ac3fb9b015fe6b409c6d
Adlan <adlanyandarbiev@gmail.com>
2019-09-25 14:16:19 -0700

    Finish test cases

--- _55_findGreaterNumbers.js
+++ _55_findGreaterNumbers.js
@@ -15,3 +15,8 @@ function findGreaterNumbers(array){
   }
   return count;
 }
+
+console.log(findGreaterNumbers([1,2,3])); // 3 (2 > 1, 3 > 2, and 3 > 1)
+console.log(findGreaterNumbers([6,1,2,7])); // 4
+console.log(findGreaterNumbers([5,4,3,2,1])); // 0
+console.log(findGreaterNumbers([])); // 0
