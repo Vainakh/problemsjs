02c38a4aff0e7296f3a512166eaf661b14f37d52
Adlan <adlanyandarbiev@gmail.com>
2019-09-25 14:14:51 -0700

    Add problem description

---
+++ _55_findGreaterNumbers.js
@@ -0,0 +1,3 @@
+Write a function called findGreaterNumbers which accepts an array and returns the number of times a number is followed by a larger number.
+
+Note that the numbers don't need to be next to each other in the array. Any pair where the second number comes later in the array and is also the larger number should count.
