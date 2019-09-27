a1396bcf54b795e76ba69d5490424a358f7c1768
Adlan <adlanyandarbiev@gmail.com>
2019-09-27 13:36:42 -0700

    Numerate and Add problem description

---
+++ _56_numInversions.js
@@ -0,0 +1,5 @@
+Given an array of numbers (not necessarily unique values), your goal is to measure how close the array is to being sorted by counting the number of inversions it has. An inversion occurs when two numbers in the array are out of order.
+
+Implement a function called numInversions which counts the number of inversions in the array.
+
+Examples:
