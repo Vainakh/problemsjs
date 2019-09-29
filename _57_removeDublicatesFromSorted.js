8b02b0f69f9534759c5b64d33023011ab582f576
Adlan <adlanyandarbiev@gmail.com>
2019-09-28 18:03:45 -0700

    Finish function body

--- _57_removeDublicatesFromSorted.js
+++ _57_removeDublicatesFromSorted.js
@@ -1,3 +1,23 @@
 Given a sorted array of numbers, return the length of the subarray formed by allowing each number to appear at most two times.
 
 Examples:
+
+function removeDuplicatesFromSorted(array) {
+  let length = 0;
+  let currentElement = 0;
+  let index = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (currentElement !== array[i]) {
+        currentElement = array[i];
+          length += 1;
+          index = 1;
+        } else {
+          index += 1;
+          if (index < 3){
+          length += 1;
+        }
+     }
+  }
+  return length;
+}
