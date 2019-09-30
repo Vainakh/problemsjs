d184013513731a4d8a156509ca962cecf9b4024d
Adlan <adlanyandarbiev@gmail.com>
2019-09-30 10:56:10 -0700

    Refacture function body

--- _60_charCountUsingRegularExpression.js
+++ _60_charCountUsingRegularExpression.js
@@ -13,3 +13,22 @@ function charCount(str){
   }
   return obj;
 }
+
+
+function charCount(str){
+  let obj = {};
+
+  for (let char of str){
+      char = char.toLowerCase();
+    if(/[a-z0-9]/.test(char)){
+      if(obj[char] > 0){
+        obj[char] += 1;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  }
+  return obj;
+}
+
+charCount("Hello hi")
