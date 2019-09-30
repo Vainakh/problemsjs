665c4e4f529d378cf8243dde2b977718636984aa
Adlan <adlanyandarbiev@gmail.com>
2019-09-30 10:28:44 -0700

    Finish body function

--- _59_rankingSystem.js
+++ _59_rankingSystem.js
@@ -1,3 +1,17 @@
 Given an array of unique numbers, return a new array of rankings of the original array. For example, if your input array is [10, 5, 20], the output should be [2, 3, 1], since 10 is the second largest number, 5 is the third largest, and 20 is the largest.
 
 Examples:
+
+function rankings(arr) {
+  // add whatever parameters you deem necessary....good luck!
+  let newArray = [];
+  let sorted = arr.slice().sort((a, b) => b - a);
+    for (let i = 0; i < arr.length; i += 1) {
+      for (let j = 0; j < sorted.length; j += 1){
+        if(arr[i] === sorted[j]){
+          newArray.push(j + 1);
+        }
+      }
+    }
+  return newArray;
+}
