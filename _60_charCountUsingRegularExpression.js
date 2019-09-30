05beb279216eed6b765fe8723ecb08aa25ab24d9
Adlan <adlanyandarbiev@gmail.com>
2019-09-30 10:52:55 -0700

    Finish example using regular expression

---
+++ _60_charCountUsingRegularExpression.js
@@ -0,0 +1,15 @@
+function charCount(str){
+  let obj = {};
+
+  for (let i = 0; i < str.length; i += 1){
+    let char = str[i].toLowerCase();
+    if(/[a-z0-9]/.test(char)){
+      if(obj[char] > 0){
+        obj[char] += 1;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  }
+  return obj;
+}
