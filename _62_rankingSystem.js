e9a482eb5b35b20c05610b9edb7ce957568e2b3d
Adlan <adlanyandarbiev@gmail.com>
2019-10-01 13:22:47 -0700

    Add description

---
+++ _62_rankingSystem.js
@@ -0,0 +1,3 @@
+Given an array of unique numbers, return a new array of rankings of the original array. For example, if your input array is [10, 5, 20], the output should be [2, 3, 1], since 10 is the second largest number, 5 is the third largest, and 20 is the largest.
+
+Examples:
