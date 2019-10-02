88fcc0946445e54ff414156eb7d0cf120384a04f
Adlan <adlanyandarbiev@gmail.com>
2019-10-02 10:43:30 -0700

    Write problem definition

---
+++ _63_objectCompare.js
@@ -0,0 +1,7 @@
+Write a function called objectCompare which accepts two objects or two arrays. The function should return true if the two objects are identical or if the two arrays contain the same values at the same indices.
+
+If you are comparing two objects, the of the keys in each object does not matter. If you are comparing two arrays the order does matter.
+
+Do not use JSON.stringify to solve this quickly - it will not work!
+
+Examples:
