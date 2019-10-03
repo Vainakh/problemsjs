b9c2c6ae3b85ef736853a43a7c6c1d30569a39e4
Adlan <adlanyandarbiev@gmail.com>
2019-10-03 15:24:15 -0700

    Add problem description

---
+++ _64_rangeInArray.js
@@ -0,0 +1,3 @@
+Write a function called rangeInArray which accepts an array and start and end indices, and returns the sum of the values between (and including) the start and end index.
+
+If a start parameter is not passed in, it should default to zero. If an end parameter is not passed in, it should default to the last value in the array. Also, if the end argument is too large, the sum should still go through the end of the array.
