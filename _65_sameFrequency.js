2e32ddb2fae7669b87cddc6e7cb2e670add91024
Adlan <adlanyandarbiev@gmail.com>
2019-10-04 19:01:49 -0700

    Finish function body

---
+++ _65_sameFrequency.js
@@ -0,0 +1,26 @@
+Write a function called sameFrequency which accepts two numbers and returns true if they contain the same frequency of digits. Otherwise, it returns false.
+
+function sameFrequency(num1, num2){
+
+        let firstNum = num1.toString().split("");
+        let secondNum = num2.toString().split("");
+
+        if(firstNum.length !== secondNum.length){
+            return false;
+        }
+
+        let newObj = [];
+
+        for (let i = 0; i < firstNum.length; i += 1) {
+            newObj[i] += firstNum[i];
+        }
+
+        for (let j = 0; j < secondNum.length; j += 1) {
+            newObj[j] -= secondNum[j];
+        }
+
+        if(newObj.keys.length === 0){
+            return true;
+        }
+
+    }
