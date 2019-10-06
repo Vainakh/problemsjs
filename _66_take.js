2fe2a6bce55d58a4518388b254c83bdcc17fa4a6
Adlan <adlanyandarbiev@gmail.com>
2019-10-06 04:37:48 -0700

    Complete body function

--- _66_take.js
+++ _66_take.js
@@ -1,1 +1,16 @@
 Write a function called take which accepts an array and an optional value and creates a copy of the array starting from the beginning for the value number of elements. If a value is not passed the function should return an array with the first element. If the second parameter is greater than the length of the array, return a copy of the entire array.
+
+function take(array, optional){
+    let newArray = [];
+
+    if(!optional && optional !== 0){
+        return [array[0]];
+    }
+
+    for (let i = 0; i < array.length; i += 1) {
+        if(newArray.length < optional) {
+        newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
