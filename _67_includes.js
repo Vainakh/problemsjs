a311373ae2049418b68e5befff7ece1a5ca162f2
Adlan <adlanyandarbiev@gmail.com>
2019-10-07 16:12:46 -0700

    Finish function body

---
+++ _67_includes.js
@@ -0,0 +1,3 @@
+Write a function called includes which accepts a collection, a value, and an optional starting index. The function should return true if the value exists in the collection when we search starting from the starting index. Otherwise, it should return false.
+
+The collection can be a string, an array, or an object. If the collection is a string or array, the third parameter is a starting index for where to search from. If the collection is an object, the function searches for the value among values in the object; since objects have no sort order, the third parameter is ignored.
