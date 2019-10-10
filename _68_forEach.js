6996c8bda07c1ff26b45d42282469af88b152db1
Adlan <adlanyandarbiev@gmail.com>
2019-10-10 16:12:46 -0700

    Add instructions

---
+++ _68_forEach.js
@@ -0,0 +1,1 @@
+Write a function called forEach, which accepts an array and a callback. forEach should iterate through the array and run a callback function for each value in the array. The callback should accept as its first argument the current array element, and as its second element the current array index. Also, forEach should always return undefined.
