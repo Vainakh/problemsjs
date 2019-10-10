1bad214e7d0c8c2ecc9e0b7e744a3023f1df0ad7
Adlan <adlanyandarbiev@gmail.com>
2019-10-10 16:13:22 -0700

    Add edge case

--- _68_forEach.js
+++ _68_forEach.js
@@ -1,1 +1,4 @@
 Write a function called forEach, which accepts an array and a callback. forEach should iterate through the array and run a callback function for each value in the array. The callback should accept as its first argument the current array element, and as its second element the current array index. Also, forEach should always return undefined.
+Do not use the built in forEach function, the tests will fail!
+
+Examples:
