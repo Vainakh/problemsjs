af598a8172e7d896e762ad18b9df31b10b2b4870
Adlan <adlanyandarbiev@gmail.com>
2019-10-12 12:36:25 -0700

    Add problem description

--- _70_map.js
+++ _70_map.js
@@ -1,1 +1,4 @@
 Instructions from your teacher:
+Write a function called map which accepts an array and a callback. The function should iterate over the array passed to the function and return a new array, whose values are determined by the return values from the callback. For each iteration, the callback should accept the current value, current index and the entire original array as its arguments.
+
+Since map returns a new array, it should not mutate the original array passed into it.
