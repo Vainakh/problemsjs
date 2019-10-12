a41a5f6276d60ed7c5bcdfe4c05eee9538827937
Adlan <adlanyandarbiev@gmail.com>
2019-10-12 12:36:57 -0700

    Add edge cases

--- _70_map.js
+++ _70_map.js
@@ -2,3 +2,5 @@ Instructions from your teacher:
 Write a function called map which accepts an array and a callback. The function should iterate over the array passed to the function and return a new array, whose values are determined by the return values from the callback. For each iteration, the callback should accept the current value, current index and the entire original array as its arguments.
 
 Since map returns a new array, it should not mutate the original array passed into it.
+
+Do not use the built in map function, the tests will fail!
