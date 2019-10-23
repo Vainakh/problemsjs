c9a57629be3330f9080ce6ac8d06563a41a19b1b
Adlan <adlanyandarbiev@gmail.com>
2019-10-22 19:13:19 -0700

    Numerate and provide instructions

---
+++ _73_every.js
@@ -0,0 +1,7 @@
+Write a function called every which accepts an array and a callback. every should return true if the result of the callback at each value is truthy; otherwise it should return false. The callback accepts as its arguments the current array element, the current array index, and the entire array.
+
+every should not mutate the input array.
+
+Do not use the built in every function, the tests will fail!
+
+Examples:
