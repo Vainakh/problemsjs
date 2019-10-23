8051adf711f20af36836e3b7b0c864f74051e3ce
Adlan <adlanyandarbiev@gmail.com>
2019-10-22 19:14:08 -0700

    Add function body and test cases

--- _73_every.js
+++ _73_every.js
@@ -5,3 +5,34 @@ every should not mutate the input array.
 Do not use the built in every function, the tests will fail!
 
 Examples:
+
+
+  function every(array, callback){
+
+    for (let i = 0; i < array.length; i += 1){
+        if(!callback(array[i], i, array)){
+            return false;
+        }
+    }
+    return true;
+}
+
+var arr = [1, 2, 3, 4];
+
+console.log(every(arr, function (val, idx, arr) {
+    return val < 5;
+})); // true
+
+console.log(every(arr, function (val, idx, arr) {
+    return val > 4;
+})); // false
+
+console.log(every(arr, function (val, idx, arr) {
+    return idx < 5;
+})); // true
+
+console.log(every(arr, function (val, idx, arr) {
+    return arr.indexOf(4) > -1;
+})); // true
+
+console.log(arr); // [1,2,3,4]
