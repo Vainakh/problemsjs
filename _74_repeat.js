eafa9729719844905d0a1bdadadbda40312d1d3c
Adlan <adlanyandarbiev@gmail.com>
2019-10-23 17:24:00 -0700

    Finish function body

--- _74_repeat.js
+++ _74_repeat.js
@@ -1,1 +1,10 @@
-Write a function called repeat, which accepts a string and a number and returns a new string with the string passed to the function repeated the number amount of times. Do not use the built in repeat method!
+Write a function called repeat, which accepts a string and a number and returns a new string with the string passed to the function repeated the number amount of times. Do not use the built in repeat method!\
+function repeat(string, number){
+    let newString = "";
+
+    while (number > 0){
+    newString += string;
+    number -= 1;
+    }
+    return newString;
+}
