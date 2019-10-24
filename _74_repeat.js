8fcced728fcb195e3481988cca6f1450b5c5330b
Adlan <adlanyandarbiev@gmail.com>
2019-10-23 17:23:27 -0700

    Numerate and add problem instruction

---
+++ _74_repeat.js
@@ -0,0 +1,1 @@
+Write a function called repeat, which accepts a string and a number and returns a new string with the string passed to the function repeated the number amount of times. Do not use the built in repeat method!
