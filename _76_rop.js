2d0342337f06483ae03d598ad08a0633ee3a1b9e
Adlan <adlanyandarbiev@gmail.com>
2019-10-26 19:36:39 -0700

    Numerate and instructions

---
+++ _76_rop.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Write a function called drop which accepts an array and optional number and returns a new array with the number of elements dropped from the beginning. If the number is greater than the length of the array, return an empty array and if there is no second parameter, return a slice starting from the first index to the end of the array.
+
+Since drop returns a new array, it should not mutate the original array.
