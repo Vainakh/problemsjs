775530dbc978e440d35317fc3898eb8f72481e34
Adlan <adlanyandarbiev@gmail.com>
2019-10-29 16:01:27 -0700

    Add test cases

--- _77_tap.js
+++ _77_tap.js
@@ -25,3 +25,18 @@ console.log(tap([1, 2, 3], function (array) {
 console.log(tap(["a", "b", "c"], function (array) {
     return array[0];
 })); // ["a","b","c"]
+
+
+tap([1, 2, 3], function(array) {
+  array.pop();
+}).reverse(); // [2,1]
+
+tap([1, 2, 3], function(array) {
+  array[0] = 10
+  array[1] = -10
+  return "cool";
+}); // [10, -10, 3];
+
+tap(["a","b","c"], function(array) {
+  return array[0];
+}); // ["a","b","c"]
