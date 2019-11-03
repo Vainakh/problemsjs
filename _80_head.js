1370d0fad6da46b3fea0c47073c1e8ca0c62417e
Adlan <adlanyandarbiev@gmail.com>
2019-11-02 17:29:00 -0700

    Finish function body

--- _80_head.js
+++ _80_head.js
@@ -1,1 +1,13 @@
 Write a function called head which returns the first element of the input array.
+
+function head(array){
+    return array[0];
+}
+
+console.log(head([1, 2, 3])); // 1
+console.log(head([
+    { name: "Elie" },
+    { name: "Matt" },
+    { name: "Michael" }
+]));// {name: "Elie"}
+console.log(head([])); // undefined
