934969a275da6fac8ae50c04ea30fd28913e03d9
Adlan <adlanyandarbiev@gmail.com>
2019-11-03 18:29:00 -0800

    Finish function body

--- _81_isNil.js
+++ _81_isNil.js
@@ -1,1 +1,8 @@
 Write a function called isNil, which should return true if the input is null or undefined.
+function isNil(value){
+
+    if(value === undefined || value === NaN || value === null) {
+        return true;
+    }
+    return false;
+}
