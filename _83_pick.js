bd282208895168388cdeb31867b5da2fc9fd5f81
Adlan <adlanyandarbiev@gmail.com>
2019-11-06 14:01:08 -0800

    Numerate and add instructions

---
+++ _83_pick.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called pick, which accepts an object and an array of keys and returns a new object with all of the keys that are in the array passed to pick. If an array element doesn't correspond to a valid key, the element is simply ignored.
