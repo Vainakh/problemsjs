8acfa1ddbbcca213c964d4a4d230a2f8b10c541d
Adlan <adlanyandarbiev@gmail.com>
2019-11-07 19:36:43 -0800

    Finish function body

--- _84_flatten.js
+++ _84_flatten.js
@@ -1,2 +1,14 @@
 nstructions from your teacher:
 Write a function called flatten, which flattens an array a single level deep. In other words, if you have one level of nesting in your arrays, it will remove this one level of nesting.
+function flatten(array) {
+
+        var newArray = [];
+        for (var i = 0; i < array.length; i += 1) {
+            if (Array.isArray(array[i])) {
+                newArray = newArray.concat((array[i]));
+            } else {
+                newArray.push(array[i]);
+            }
+        }
+        return newArray;
+    }
