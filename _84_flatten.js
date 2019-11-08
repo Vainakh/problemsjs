54054ed81826202fcb978968f6626c0c41e8d657
Adlan <adlanyandarbiev@gmail.com>
2019-11-07 19:36:09 -0800

    Numerate and add instructions

---
+++ _84_flatten.js
@@ -0,0 +1,2 @@
+nstructions from your teacher:
+Write a function called flatten, which flattens an array a single level deep. In other words, if you have one level of nesting in your arrays, it will remove this one level of nesting.
