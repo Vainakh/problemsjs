a0fe5d3d9f86fd518b3b376724adcf5b45d5c128
Adlan <adlanyandarbiev@gmail.com>
2019-11-08 18:13:16 -0800

    Numerate and add instructions

---
+++ _85_clamp.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called clamp, which accepts three parameters: a number, a lower bound, and an upper bound. The function should return the number if it is in between the lower and upper bounds. Otherwise, the function should return lower if number is less than lower, or upper if number is greater than upper.
