8aa9eede89b6e3ea20be3d874d8d429b91c71de2
Adlan <adlanyandarbiev@gmail.com>
2019-11-08 18:14:01 -0800

    Finish body function and add test cases

--- _85_clamp.js
+++ _85_clamp.js
@@ -1,2 +1,25 @@
 Instructions from your teacher:
 Write a function called clamp, which accepts three parameters: a number, a lower bound, and an upper bound. The function should return the number if it is in between the lower and upper bounds. Otherwise, the function should return lower if number is less than lower, or upper if number is greater than upper.
+
+function clamp(number, lower, upper){
+
+    if(number === 0 || lower === 0 || upper === 0) {
+        return 0;
+    }
+
+    for (let i = lower; i < upper; i += 1) {
+        if(number > lower && number < upper) {
+            return number;
+        } else if(number < lower) {
+            return lower;
+        } else {
+            return upper;
+        }
+    }
+}
+
+console.log(clamp(-10, -5, 6)); // -5
+console.log(clamp(10, -2, 5)); // 5
+console.log(clamp(2, -5, 5)); // 2
+console.log(clamp(-2, -5, 5)); // -2
+console.log(clamp(0, 0, 0)); // 0
