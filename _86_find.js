f620f87bc36ba5ed22c52cca336c027e44482923
Adlan <adlanyandarbiev@gmail.com>
2019-11-09 16:46:10 -0800

    Finish function

--- _86_find.js
+++ _86_find.js
@@ -1,2 +1,11 @@
 Instructions from your teacher:
 Write a function called find which accepts an array and a callback. The function should return the first value for which the callback returns a truthy value. If the callback never returns a truthy value, the function should return undefined.
+
+function find(array, callback){
+
+    for (let i = 0; i < array.length; i += 1) {
+        if(callback(array[i])){
+            return array[i];
+        }
+    }
+}
