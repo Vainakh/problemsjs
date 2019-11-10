6e4bed4a0bc234fa890fcd9ff36c7be24e5a94eb
Adlan <adlanyandarbiev@gmail.com>
2019-11-09 16:45:19 -0800

    Numerate and add instructions

---
+++ _86_find.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called find which accepts an array and a callback. The function should return the first value for which the callback returns a truthy value. If the callback never returns a truthy value, the function should return undefined.
