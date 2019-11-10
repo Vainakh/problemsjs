8371e3a6c7c01b3b5c5468ac1fa5ba66ef1fc49e
Adlan <adlanyandarbiev@gmail.com>
2019-11-09 16:46:50 -0800

    Add test cases

--- _86_find.js
+++ _86_find.js
@@ -9,3 +9,15 @@ function find(array, callback){
         }
     }
 }
+
+console.log(find([1, 2, 3, 4], function (value) {
+    return value === 2;
+})); // 2
+
+console.log(find([{ name: 'Test' }, { name: 'Another' }], function (value) {
+    return value.name === 'Another';
+})); // {name: 'Another'}
+
+console.log(find(['a', 'bb', 'ccc'], function (value) {
+    return value.length > 3;
+})); // undefined
