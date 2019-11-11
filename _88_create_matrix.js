490728503ded29032d15a87261229235345096d8
Adlan <adlanyandarbiev@gmail.com>
2019-11-11 15:41:35 -0800

    Numerate and add instruction

---
+++ _88_create_matrix.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Writ a function called createMatrix which takes in two whole numbers, n and m, and creates a two-dimensional array with n columns and m rows. All of the values inside of your array should be 0.
