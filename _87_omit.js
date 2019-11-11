e1437406f1d149f302a5f414f9acf6771fd101f2
Adlan <adlanyandarbiev@gmail.com>
2019-11-11 15:39:07 -0800

    Finish function body

--- _87_omit.js
+++ _87_omit.js
@@ -1,1 +1,13 @@
 Write a function called omit, which accepts an object and an array of keys and returns a new object with the keys from the array omitted. If an array element doesn't correspond to a valid key, the element is simply ignored.
+function omit(object, keys){
+    let newObject = {};
+
+    let newKeys = Object.keys(object);
+
+    for (let i = 0; i < newKeys.length; i += 1){
+        if(!keys.includes(newKeys[i])){
+            newObject[newKeys[i]] = object[newKeys[i]]
+        }
+    }
+    return newObject;
+}
