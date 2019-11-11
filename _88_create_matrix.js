1480fae0749150095ec484fdbd485e3aed7292de
Adlan <adlanyandarbiev@gmail.com>
2019-11-11 15:42:13 -0800

    Finish body function

--- _88_create_matrix.js
+++ _88_create_matrix.js
@@ -1,2 +1,15 @@
 Instructions from your teacher:
 Writ a function called createMatrix which takes in two whole numbers, n and m, and creates a two-dimensional array with n columns and m rows. All of the values inside of your array should be 0.
+
+function createMatrix(n, m){
+
+    let newArray = [];
+
+    for (let i = 0; i < m; i += 1) {
+        newArray[i] = [];
+        for (let j = 0; j < n; j += 1) {
+            newArray[i][j] = 0;
+        }
+    }
+    return newArray;
+}
