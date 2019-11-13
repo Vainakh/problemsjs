780f534bd58ac99ce043788789e77d324bd738f3
Adlan <adlanyandarbiev@gmail.com>
2019-11-12 19:26:48 -0800

    Numerate and add instructions

---
+++ 89_fill.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called fill that accepts an array and a value and returns the same array with each slot in the array filled with the value passed to fill.
