f1d4bdfeaa88129776e66f71534f8fea28e35129
Adlan <adlanyandarbiev@gmail.com>
2019-11-13 14:01:25 -0800

    Numerate and add instructions from your teacher

---
+++ _90_isPrime.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Write a function called isPrime which returns true if a number is a prime number.
+
+A prime number is a number greater than 1 which is only divisible by one and itself. The first few primes are 2, 3, 5, 7, 11, 13, and 17. Notice that 4 is not prime, since 4 = 2 * 2. Similarly, 6 is not prime since 6 = 2 * 3.
