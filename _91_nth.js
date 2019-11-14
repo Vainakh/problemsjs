52925ebc63423b5639f152adac0cd8a733295a38
Adlan <adlanyandarbiev@gmail.com>
2019-11-14 06:07:17 -0800

    Numerate and add instructions from teacher

---
+++ _91_nth.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Write a function called nth, which accepts an array and a number and returns the element at whatever index is the number in the array. If the number is negative, the nth element from the end is returned.
+
+You can assume that number will always be between the negative value of the array length, and the array length minus 1.
