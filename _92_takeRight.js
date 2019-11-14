68f0456aa16dabeae5285aef88b260f1ba9ebd9d
Adlan <adlanyandarbiev@gmail.com>
2019-11-14 06:09:24 -0800

    Numerate and add instructions from your teacher

---
+++ _92_takeRight.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called takeRight which accepts an array and an optional value and creates a copy of the array starting from the end for the value number of elements. If a value is not passed the function should return an array with the last element. If the second parameter is greater than the length of the array, return a copy of the entire array.
