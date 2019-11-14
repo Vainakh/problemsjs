fa7b21e6e7c840b1c1f762624b752d72cf0b63b1
Adlan <adlanyandarbiev@gmail.com>
2019-11-14 06:10:08 -0800

    Finish body function and add test cases

--- _92_takeRight.js
+++ _92_takeRight.js
@@ -1,2 +1,28 @@
 Instructions from your teacher:
 Write a function called takeRight which accepts an array and an optional value and creates a copy of the array starting from the end for the value number of elements. If a value is not passed the function should return an array with the last element. If the second parameter is greater than the length of the array, return a copy of the entire array.
+function takeRight(array, value){
+    let newArray = [];
+
+    if(value === 0){
+        return [];
+    }
+
+    if(value === undefined){
+        return [array[array.length - 1]];
+    }
+
+    if(value > array.length) {
+        return array;
+    }
+
+    for (let i = 0; i < array.length; i += 1) {
+        newArray.push(array[i]);
+    }
+
+    return newArray.splice(-value)
+}
+
+console.log(takeRight([1, 2, 3])); // [3]
+console.log(takeRight([1, 2, 3], 2)); // [2, 3]
+console.log(takeRight([1, 2, 3], 5)); // [1, 2, 3]
+console.log(takeRight([1, 2, 3], 0)); // []
