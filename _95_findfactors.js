11530231a3abb6ded438b363fa9e57feba9dfb17
Adlan <adlanyandarbiev@gmail.com>
2019-11-17 09:34:49 -0800

    Numerate and add teachers instructions

---
+++ _95_findfactors.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called findFactors which accepts a number and returns an array of all of the numbers which it is divisible by starting from 1 and going up to the number.
