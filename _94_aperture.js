810ba9707a7674389599ca54f503ca482d7552c4
Adlan <adlanyandarbiev@gmail.com>
2019-11-16 17:35:30 -0800

    Numerate and add instructions from teacher

---
+++ _94_aperture.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called aperture which accepts a number and an array and returns a new array. The new array should be composed of subarrays the size of the number with consecutive elements. If the number passed to this function is greater than the length of the array or less than 1, an empty array is returned.
