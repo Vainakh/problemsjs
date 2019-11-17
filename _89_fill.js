2163f9a1142e93c7700144e2bd265f26feee49ea
Adlan <adlanyandarbiev@gmail.com>
2019-11-16 17:33:28 -0800

    Finish body function and add test cases

--- _89_fill.js
+++ _89_fill.js
@@ -15,6 +15,6 @@ console.log(fill(arr, "y"));
 console.log(arr);// ["y","y","y"]
 
 console.log(fill([undefined, undefined, null, false], "1"));
-// ["1","1","1","1"]
+// ["1","1","1","1"]notours
 console.log(fill([2, 3, 1, 95], 4)); // [4,4,4,4]
 console.log(fill([1, 2, 3, 4, 5], 5)); // [5,5,5,5,5]
--- _93_upperfirst.js
+++ _93_upperfirst.js
@@ -1,2 +1,13 @@
 Instructions from your teacher:
 Write a function called upperFirst, which converts the first character of an input string to upper case.
+
+function upperFirst(string){;
+
+    return newString = string.charAt(0).toUpperCase() + string.slice(1);
+
+}
+
+console.log(upperFirst('fred')); // 'Fred'
+console.log(upperFirst('FRED')); // 'FRED'
+console.log(upperFirst('')); // ''
+console.log(upperFirst('only first character')); // 'Only first character'
