ab42918c58a697fa420744a2b98975c6bea9a03d
Adlan <adlanyandarbiev@gmail.com>
2019-11-18 17:43:31 -0800

    Numerate and add instructions from teacher

---
+++ _96_greatestcommondiv.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called greatestCommonDivisor which accepts two whole numbers and returns the greatest common divisor between both numbers. The greatest common divisor of two whole numbers is the largest whole number that divides both numbers evenly.
