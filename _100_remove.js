e0d3b10580623d443632635b02749aadcd324632
Adlan <adlanyandarbiev@gmail.com>
2019-11-20 12:38:43 -0800

    Numerate and add instructions from teacher

---
+++ _100_remove.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Write a function called remove that accepts an array and a callback. This function should iterate through the array and remove all elements for which the callback returns a truthy value. Then, the function should return an array of removed elements.
+
+The callback should accept up to three arguments: the current array value, the current array index, and the entire array.
