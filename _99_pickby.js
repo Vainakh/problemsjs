9236c994d038c1f1ec4b035c23e03387d75dfa88
Adlan <adlanyandarbiev@gmail.com>
2019-11-20 12:29:45 -0800

    Numerate and add teachers instructions

---
+++ _99_pickby.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called pickBy which accepts an object and a callback function. The function should return a new object consisting of keys and values where the value returns something truthy when passed into the callback.
