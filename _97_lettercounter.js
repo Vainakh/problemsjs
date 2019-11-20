848505999407e72730baf0a813b98828dc2a2209
Adlan <adlanyandarbiev@gmail.com>
2019-11-19 20:35:38 -0800

    Numerate and add instructions from the teacher

---
+++ _97_lettercounter.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called letterCounter which accepts a string and returns a function. When the inner function is invoked it should accept a parameter which is a letter, and the inner function should return the number of times that letter appears. This inner function should be case insensitive.
