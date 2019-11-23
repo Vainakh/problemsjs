7e2cb8dfb0daa164483cd095ad96f9076e85c8ae
Adlan <adlanyandarbiev@gmail.com>
2019-11-22 19:18:22 -0800

    Numerate problem and add teachers instructions

---
+++ __101_sumby.js
@@ -0,0 +1,6 @@
+Instructions from your teacher:
+Write a function called sumBy which accepts an array of objects and a rule for summing up those objects to obtain a value.
+
+The rule can be provided in one of two ways. One way is to provide a callback function which is invoked for each element in array to generate the value to be summed. The callback is invoked with one argument: the current array element.
+
+Another option for the rule is to pass in a string referencing a key. With this syntax, the value to be summed is the value corresponding to the key passed in.
