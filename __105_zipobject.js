1626fe6428fa38eca5813851dd4463efb94d9460
Adlan <adlanyandarbiev@gmail.com>
2019-11-24 07:25:44 -0800

    Numerate and add teacher instructions

---
+++ __105_zipobject.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called zipObject which accepts two arrays, one of property identifiers and one of corresponding values. It returns an object with the keys from one array and values from another. If the first array is longer than the second, the value in the object should be undefined. If the second array is longer, ignore the additional values in the second array.
