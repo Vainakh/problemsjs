fa36495ac026b82ab8ec3da1f649c13771182a92
Adlan <adlanyandarbiev@gmail.com>
2019-11-25 16:10:53 -0800

    Finish function body

--- __106_binarysearch.js
+++ __106_binarysearch.js
@@ -2,3 +2,46 @@ Instructions from your teacher:
 Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
 
 This algorithm should be more efficient than linearSearch - you can read how to implement it here - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+
+
+function binarySearch(array, target){
+
+    let left = 0;
+    let right = array.length - 1;
+    let middle = Math.floor((right + left) / 2);
+
+    if(right < left){
+        return -1;
+    }
+
+    while (array[middle] !== target && left < right) {
+
+        if (target < array[middle]) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+        middle = Math.floor((left + right) / 2);
+    }
+
+    return (array[middle] === target) ? middle : -1;
+
+    }
+
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10)); // 2
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95)); // 16
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 98
+], 100)); // -1
