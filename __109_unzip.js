4fbe09c99f3234fa3d84db2c8fbae91cd1b945a7
Adlan <adlanyandarbiev@gmail.com>
2019-11-26 06:24:02 -0800

    Numerate and add instructions

---
+++ __109_unzip.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called unzip, which accepts an array of grouped elements and returns a new an array, which regroups the elements to their pre-zip configuration.
