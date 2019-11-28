c8e4e08932fa7018cf58aab52308f9bb4dfa7c22
Adlan <adlanyandarbiev@gmail.com>
2019-11-28 06:52:47 -0800

    Finish function body and add test cases

--- __111_doublevalues.js
+++ __111_doublevalues.js
@@ -1,2 +1,14 @@
 Instructions from your teacher:
 Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled.
+
+function doubleValues(array){
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i += 1) {
+        newArray.push(array[i] * 2);
+    }
+    return newArray;
+}
+
+console.log(doubleValues([1, 2, 3])); // [2,4,6]
+console.log(doubleValues([5, 1, 2, 3, 10])); // [10,2,4,6,20]
