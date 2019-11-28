48283bd35586cffeb8e100c9f69cd9f00a566e85
Adlan <adlanyandarbiev@gmail.com>
2019-11-28 06:49:26 -0800

    Numerate and add teachers instructions

---
+++ __110_zip.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called zip which accepts an arbitrary number of arrays, and creates an array of grouped elements, the first of which contains the first elements of the input arrays, the second of which contains the second elements of the input arrays, and so on.
