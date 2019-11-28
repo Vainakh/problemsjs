e1d4e500d470b2345ac30235fe2325e5797c6a2e
Adlan <adlanyandarbiev@gmail.com>
2019-11-28 06:50:06 -0800

    Finish function body and add test cases

--- __110_zip.js
+++ __110_zip.js
@@ -1,2 +1,26 @@
 Instructions from your teacher:
 Write a function called zip which accepts an arbitrary number of arrays, and creates an array of grouped elements, the first of which contains the first elements of the input arrays, the second of which contains the second elements of the input arrays, and so on.
+
+function zip(...array) {
+
+    let newarray = [];
+
+    for (let row = 0; row < array.length; row += 1) {
+        for (let col = 0; col <= array[row].length - 1; col += 1) {
+            let subarray = [];
+
+        for (let ele = 0; ele < array.length; ele += 1) {
+                subarray.push(array[ele][col]);
+            }
+        newarray.push(subarray);
+
+    }
+        break;
+}
+    let filteredarray = newarray.filter(function (element) {
+        return element !== undefined;
+    });
+
+
+    return filteredarray;
+}
