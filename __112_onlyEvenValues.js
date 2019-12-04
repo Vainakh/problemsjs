7d231ac4ece8e879a16a29931e140079c54f0707
Adlan <adlanyandarbiev@gmail.com>
2019-12-03 18:47:47 -0800

    Numerate and add teachers instructions Finish function body

---
+++ __112_onlyEvenValues.js
@@ -0,0 +1,16 @@
+Instructions from your teacher:
+Write a function called onlyEvenValues which accepts an array and returns a new array with only the even values in the array passed to the function
+
+function onlyEvenValues(array) {
+    let evenNumArray = [];
+
+    for (let i = 0; i < array.length; i += 1) {
+        if(array[i] % 2 === 0) {
+            evenNumArray.push(array[i]);
+        }
+    }
+    return evenNumArray;
+}
+console.log(onlyEvenValues([1, 2, 3])); // [2]
+console.log(onlyEvenValues([5, 1, 2, 3, 10])); // [2,10]
+console.log(onlyEvenValues([1, 3, 5])); // []
--- __140_isSubsequence.js
+++ __140_isSubsequence.js
@@ -1,3 +1,9 @@
+Write a function called isSubsequnce which takes in two strings and
+whether the characters in the first string form a subsequence of the
+characters in the second string. In other words, the function should
+check whether the characters in the first string appear somewhere in
+the second string, without their order changing.
+
 function isSubsequence(string1, string2) {
 
     if (string1.length === 0) return true;
