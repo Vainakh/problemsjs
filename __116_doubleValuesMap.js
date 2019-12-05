5240316c91f79aeee6697a62c267495c20f01b38
Adlan <adlanyandarbiev@gmail.com>
2019-12-05 05:44:18 -0800

    Finish functiond body and add test cases

--- __116_doubleValuesMap.js
+++ __116_doubleValuesMap.js
@@ -1,2 +1,14 @@
 Instructions from your teacher:
 Write a function called doubleValuesMap which accepts an array and returns a new array with all the values in the array passed to the function doubled.
+
+function doubleValuesMap(array){
+    let newArray = array.map(myFunction);
+
+    function myFunction(num){
+        return num * 2;
+    }
+    return newArray;
+}
+
+console.log(doubleValuesMap([1, 2, 3])); // [2,4,6]
+console.log(doubleValuesMap([1, -2, -3])); // [2,-4,-6]
