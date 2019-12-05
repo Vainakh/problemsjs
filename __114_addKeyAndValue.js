c92990db6f78bdaf24305428458242592c202041
Adlan <adlanyandarbiev@gmail.com>
2019-12-04 18:16:21 -0800

    Finish function body

--- __114_addKeyAndValue.js
+++ __114_addKeyAndValue.js
@@ -1,2 +1,10 @@
 Instructions from your teacher:
-Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added to each object. 
+Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added to each object.
+
+function addKeyAndValue(array, key, value){
+
+    array.forEach(function(object) {
+        object[key] = value;
+    });
+    return array;
+}
