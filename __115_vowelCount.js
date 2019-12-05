77069971a3455c515c81ff926026cbeca7c686e5
Adlan <adlanyandarbiev@gmail.com>
2019-12-05 04:59:52 -0800

    Finish function body

--- __115_vowelCount.js
+++ __115_vowelCount.js
@@ -1,2 +1,16 @@
 Instructions from your teacher:
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive, so both lowercase vowels and uppercase vowels should count.
+
+
+function vowelCount(str) {
+    let words = str.toLowerCase().split("");
+    let vowels = "aeoui".split("");
+    let newObj = {};
+
+    let result = words.forEach(function(val){
+        if(vowels.includes(val)){
+        newObj[val] = (newObj[val] || 0) + 1;
+        }
+    });
+    return newObj;
+}
