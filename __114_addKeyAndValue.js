6f8c502a48c028fe62ec13791b06c1848c3ac0ed
Adlan <adlanyandarbiev@gmail.com>
2019-12-04 18:15:32 -0800

    Numerate and add teachers instructions

---
+++ __114_addKeyAndValue.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added to each object. 
