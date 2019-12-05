bfa1ccfa3b3ffef8f54ef47ffc856e300ab12757
Adlan <adlanyandarbiev@gmail.com>
2019-12-05 05:02:02 -0800

    Numerate and add instructions from teacher

---
+++ __116_doubleValuesMap.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called doubleValuesMap which accepts an array and returns a new array with all the values in the array passed to the function doubled.
