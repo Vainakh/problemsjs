0ddb960ff0dc05a20237300e45fbd2b780a54f73
Adlan <adlanyandarbiev@gmail.com>
2019-12-05 04:58:59 -0800

    Numerate and add instructions

---
+++ __115_vowelCount.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive, so both lowercase vowels and uppercase vowels should count.
