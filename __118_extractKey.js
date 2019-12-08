f77aec50082f3846e96970d8e8bd8be077c3b360
Adlan <adlanyandarbiev@gmail.com>
2019-12-07 18:58:41 -0800

    Numerate and add instructions

---
+++ __118_extractKey.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called extractKey which accepts an array of objects and some key and returns a new array with the values for that key in each object.
