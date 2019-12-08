2f18a6bfe71505ba9aad4162c6685efdd34eb6ac
Adlan <adlanyandarbiev@gmail.com>
2019-12-07 18:59:16 -0800

    Finish function

--- __118_extractKey.js
+++ __118_extractKey.js
@@ -1,2 +1,11 @@
 Instructions from your teacher:
 Write a function called extractKey which accepts an array of objects and some key and returns a new array with the values for that key in each object.
+
+function extractKey(array, key) {
+    let newarray = array.map(myFunction);
+
+    function myFunction(object) {
+        return object[key];
+    }
+    return newarray;
+}
