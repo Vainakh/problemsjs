c544dfaa49e1dff647d1913066022a0a55e54b52
Adlan <adlanyandarbiev@gmail.com>
2019-12-09 16:12:28 -0800

    Numerate and add instructions

--- __120_filterByKey.js
+++ __120_filterByKey.js
@@ -1,2 +1,16 @@
 Instructions from your teacher:
 Write a function called filterByKey which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
+
+function filterByKey(array, key) {
+    let newarray = [];
+
+    for (let i = 0; i < array.length; i += 1) {
+        let currentObject = array[i];
+        let currentKeyArray = Object.keys(currentObject);
+        if(currentKeyArray.includes(key)) {
+            newarray.push(currentObject);
+        }
+
+    }
+    return newarray;
+};
