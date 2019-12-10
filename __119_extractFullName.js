a0e36cb1a10f5921539cb3271bc6936bcd79c802
Adlan <adlanyandarbiev@gmail.com>
2019-12-09 16:08:58 -0800

    Finish functions body

--- __119_extractFullName.js
+++ __119_extractFullName.js
@@ -1,2 +1,12 @@
 nstructions from your teacher:
-Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of "first" and the value of a key with the name of  "last" in each object, concatenated together with a space. 
+Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of "first" and the value of a key with the name of  "last" in each object, concatenated together with a space.
+
+function extractFullName(array){
+    let newarray = array.map(myFunction);
+
+    function myFunction(object){
+
+            return object["first"] + " " + object["last"];
+        }
+    return newarray;
+}
