309780bef43164ed0eb503c899cad1e9aa359e83
Adlan <adlanyandarbiev@gmail.com>
2019-12-13 20:23:33 -0800

    Numerate and add teachers instruction

---
+++ __121_find.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter, or undefined if the value is not found in the array.
