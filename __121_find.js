ab111f25385a3f35aadab64b6fa7495ae9213391
Adlan <adlanyandarbiev@gmail.com>
2019-12-13 20:29:20 -0800

    Finish function body

--- __121_find.js
+++ __121_find.js
@@ -1,2 +1,12 @@
 Instructions from your teacher:
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter, or undefined if the value is not found in the array.
+
+function find(array, value) {
+
+    let match = array.filter(element => element === value);
+    if (match.length !== 0) {
+        return match;
+    } else {
+        return undefined;
+    }
+}
