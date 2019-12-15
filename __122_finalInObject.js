80a292ecbb5c8b31a1410a747cf2def8346eb3c9
Adlan <adlanyandarbiev@gmail.com>
2019-12-15 09:40:38 -0800

    Finish function body

--- __122_finalInObject.js
+++ __122_finalInObject.js
@@ -1,2 +1,14 @@
 Instructions from your teacher:
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for, and returns the first found value in the array. If no match is found, return undefined.
+
+function findInObj(array, key, value) {
+    let match = array.filter(object => object[key] === value);
+
+    for (let i = 0; i < match.length; i += 1){
+       if(match[i]){
+         return match[i];
+        } else {
+        return undefined;
+    }
+}
+}
