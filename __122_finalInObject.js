daf108f73ab95ebbce068b01864572f10dce1930
Adlan <adlanyandarbiev@gmail.com>
2019-12-15 09:40:03 -0800

    Numerate and add instructions from teacher

---
+++ __122_finalInObject.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called findInObj which accepts an array of objects, a key, and some value to search for, and returns the first found value in the array. If no match is found, return undefined.
