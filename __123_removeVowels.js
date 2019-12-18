6ab7c1418b31135833548391508b20f6ac43ff9f
Adlan <adlanyandarbiev@gmail.com>
2019-12-17 17:48:40 -0800

    Finish function body

--- __123_removeVowels.js
+++ __123_removeVowels.js
@@ -1,2 +1,15 @@
 Instructions from your teacher:
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
+
+function removeVowels(string){
+    let lowered = string.toLowerCase();
+    let word = lowered.split("");
+
+
+    let modified = word.filter(function(char) {
+        let vowels = "aeuioAEUIO";
+        let constants = !vowels.includes(char);
+        return constants;
+    });
+    return modified.join("")
+}
