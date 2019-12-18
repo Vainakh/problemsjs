4096c2bbe4bb974d44923b37a875a0f91a55ce71
Adlan <adlanyandarbiev@gmail.com>
2019-12-17 17:51:31 -0800

    Finish function body

--- __124_doubleOddNumbers.js
+++ __124_doubleOddNumbers.js
@@ -1,2 +1,8 @@
 nstructions from your teacher:
 Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers in the input array doubled. (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+function doubleOddNumbers(array) {
+    let odd = (array.filter((element, i) => element % 2 !== 0));
+    let double = odd.map(x => x * 2);
+    return double;
+}
