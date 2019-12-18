4f6dbf244535f385cfca4e72a12ae520d5fe7455
Adlan <adlanyandarbiev@gmail.com>
2019-12-17 17:48:07 -0800

    Add instructions from the teacher

---
+++ __123_removeVowels.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
