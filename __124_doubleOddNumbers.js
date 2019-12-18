8551142db038f15f8faf004cee88a38a98a916ff
Adlan <adlanyandarbiev@gmail.com>
2019-12-17 17:50:57 -0800

    Numerate and add instructions from teacher

---
+++ __124_doubleOddNumbers.js
@@ -0,0 +1,2 @@
+nstructions from your teacher:
+Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers in the input array doubled. (HINT - you can use map and fitler to double and then filter the odd numbers).
