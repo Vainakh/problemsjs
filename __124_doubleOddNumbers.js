5bee5d3f78418dc273ad4e59e5a1ffe3d1be7709
Adlan <adlanyandarbiev@gmail.com>
2019-12-17 17:52:00 -0800

    Add test cases

--- __124_doubleOddNumbers.js
+++ __124_doubleOddNumbers.js
@@ -6,3 +6,7 @@ function doubleOddNumbers(array) {
     let double = odd.map(x => x * 2);
     return double;
 }
+
+console.log(doubleOddNumbers([3, 1, 1, 3])); // [6,2,2,6]
+console.log(doubleOddNumbers([1, 2, 3, 4, 5])); // [2,6,10]
+console.log(doubleOddNumbers([4, 4, 4, 4, 4])); // []
