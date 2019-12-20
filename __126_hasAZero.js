ad427b764c6d0e4d0c98c97a537c09196e0451d6
Adlan <adlanyandarbiev@gmail.com>
2019-12-19 16:08:50 -0800

    Finish fucntion body

--- __126_hasAZero.js
+++ __126_hasAZero.js
@@ -1,2 +1,13 @@
 Instructions from your teacher:
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false.
+
+function hasAZero(number){
+    let numberArray = Array.from(String(number), Number);
+
+
+    let hasZero = numberArray.some(function(element) {
+        return element === 0;
+    });
+
+    return hasZero;
+}
