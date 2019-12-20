761a2eaefc7e709d406f89ece0db4fce6e27074f
Adlan <adlanyandarbiev@gmail.com>
2019-12-19 16:06:42 -0800

    Finish function body

--- __125_hasOddNumber.js
+++ __125_hasOddNumber.js
@@ -1,2 +1,11 @@
 Instructions from your teacher:
 Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number. Otherwise, it returns false.
+
+function hasOddNumber(array) {
+
+    let odd = array.some(function(element) {
+        let uneven = element % 2 !== 0;
+        return uneven;
+    });
+    return odd;
+}
