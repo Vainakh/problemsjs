0372d0232db7925921d5ad7245c8cd0e488fec39
Adlan <adlanyandarbiev@gmail.com>
2019-12-19 16:06:12 -0800

    Numerate and add teachers instructions

---
+++ __125_hasOddNumber.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number. Otherwise, it returns false.
