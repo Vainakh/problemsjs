11230d1003a6cf30e650726f559cc03e2f9cdb9f
Adlan <adlanyandarbiev@gmail.com>
2019-12-20 07:51:50 -0800

    Numerate and add instructions

---
+++ __128_ hasNoDuplicates.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another element). If there are any duplicates, the function should return false.
