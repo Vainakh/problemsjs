24e42009584b5bd9885d3493915ae1ad97a026f9
Adlan <adlanyandarbiev@gmail.com>
2019-12-19 16:08:17 -0800

    Numerate and add instructions

---
+++ __126_hasAZero.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false.
