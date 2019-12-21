ce01f3dbeeb0a3b03027aed335a505ac52aa588c
Adlan <adlanyandarbiev@gmail.com>
2019-12-21 06:20:22 -0800

    Finish function body

--- __129_hasCertainKey.js
+++ __129_hasCertainKey.js
@@ -1,2 +1,6 @@
 Instructions from your teacher:
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
+
+function hasCertainKey(array, key){
+    return array.every(element => element[key]);
+}
