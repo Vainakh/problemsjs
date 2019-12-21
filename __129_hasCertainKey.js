439254f27f8df3b158a7625cbd284ece301f1b6b
Adlan <adlanyandarbiev@gmail.com>
2019-12-21 06:19:33 -0800

    Numerate and add instructions from teacher

---
+++ __129_hasCertainKey.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
