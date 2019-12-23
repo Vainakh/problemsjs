0f1df2fd7ed7e4da00ee55e563bcf18332d64c8b
Adlan <adlanyandarbiev@gmail.com>
2019-12-23 07:42:51 -0800

    Numerate and add instructions from teacher

---
+++ __130_hasCertainValue.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called hasCertainValue which accepts an array of objects, a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise, it should return false.
