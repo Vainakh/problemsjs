82ad428f7af9660001f97041049c5d2d02766c25
Adlan <adlanyandarbiev@gmail.com>
2019-12-23 07:44:14 -0800

    Add test cases

--- __130_hasCertainValue.js
+++ __130_hasCertainValue.js
@@ -3,3 +3,13 @@ Write a function called hasCertainValue which accepts an array of objects, a key
 function hasCertainValue(array, key, value){
     return array.every(element => element[key] === value);
 }
+
+var arr = [
+    { title: "Instructor", first: 'Elie', last: "Schoppik" },
+    { title: "Instructor", first: 'Matt', last: "Lane", isCatOwner: true },
+    { title: "Instructor", first: 'Michael', last: "Hueter" },
+    { title: "Instructor", first: 'Colt', last: "Steele", isCatOwner: true }
+]
+
+console.log(hasCertainValue(arr, 'title', 'Instructor')); // true
+console.log(hasCertainValue(arr, 'first', 'Elie')); // false
