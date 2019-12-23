e4c70a8f15cb6997a5279947e76206185710912a
Adlan <adlanyandarbiev@gmail.com>
2019-12-23 07:43:45 -0800

    Finish functions body

--- __130_hasCertainValue.js
+++ __130_hasCertainValue.js
@@ -1,2 +1,5 @@
 Instructions from your teacher:
 Write a function called hasCertainValue which accepts an array of objects, a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise, it should return false.
+function hasCertainValue(array, key, value){
+    return array.every(element => element[key] === value);
+}
