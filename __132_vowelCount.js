c3e6df09be240e7439197e4078e1d51a2a1a8497
Adlan <adlanyandarbiev@gmail.com>
2019-12-24 15:17:32 -0800

    Add test cases

--- __132_vowelCount.js
+++ __132_vowelCount.js
@@ -16,3 +16,10 @@ function vowelCount(str) {
         }
         return newObj;
     }
+
+    vowelCount('Elie') // {e:2,i:1};
+  vowelCount('Tim') // {i:1};
+  vowelCount('Matt') // {a:1})
+  vowelCount('hmmm') // {};
+  vowelCount('I Am awesome and so are you')
+  // {i: 1, a: 4, e: 3, o: 3, u: 1};
