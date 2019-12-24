b24b1a3e9e8b7c300033ba29fcd3439b3d3d455c
Adlan <adlanyandarbiev@gmail.com>
2019-12-24 15:13:21 -0800

    Numerate and add teachers instructions

---
+++ __131_sumLessThan.js
@@ -0,0 +1,2 @@
+Instructions from your teacher:
+Write a function called sumLessThan which accepts an array of numbers and a number.  The function should return the sum off all numbers less than the number.
