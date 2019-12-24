4710dac36999370d8ba2b94f972ced7a2abdb9b7
Adlan <adlanyandarbiev@gmail.com>
2019-12-24 15:14:09 -0800

    Finish functions body

--- __131_sumLessThan.js
+++ __131_sumLessThan.js
@@ -1,2 +1,12 @@
 Instructions from your teacher:
 Write a function called sumLessThan which accepts an array of numbers and a number.  The function should return the sum off all numbers less than the number.
+
+function sumLessThan(array, number){
+    let sum = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        if(array[i] < number) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
