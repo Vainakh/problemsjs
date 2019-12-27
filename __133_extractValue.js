7e1c773f9d034c5b2c78ac95e688f4c0ead749b4
Adlan <adlanyandarbiev@gmail.com>
2019-12-27 04:35:01 -0800

    Finish functions body

--- __133_extractValue.js
+++ __133_extractValue.js
@@ -1,2 +1,13 @@
 Instructions from your teacher:
 Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at that key. If there is no value at that key, no value should be placed in the output array.
+function extractValue(array, key){
+    let newarray = [];
+
+        for (let i = 0; i < array.length; i += 1) {
+            if(array[i][key] !== undefined){
+            newarray.push(array[i][key])
+        }
+    }
+
+    return newarray;
+}
