e03550e2979f80dbd946e8128e41964e6f7e2df9
Adlan <adlanyandarbiev@gmail.com>
2019-12-29 15:32:17 -0800

    Numerate and add instructions

---
+++ __134_ keyCount.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Write a function called keyCount which accepts an array of objects and returns an object with a count of how many times each key appears in the array of objects.
+
+Examples:
