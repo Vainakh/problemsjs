b2bef614212d7289475dd3f6b31d6561b11dae00
Adlan <adlanyandarbiev@gmail.com>
2019-12-29 15:32:51 -0800

    Finish fucntions body

--- __134_ keyCount.js
+++ __134_ keyCount.js
@@ -2,3 +2,13 @@ Instructions from your teacher:
 Write a function called keyCount which accepts an array of objects and returns an object with a count of how many times each key appears in the array of objects.
 
 Examples:
+
+function keyCount(array){
+
+    return array.reduce(function (current, obj, i, array) {
+        for (let key in obj) {
+            current[key] = (current[key] += 1) || 1;
+        }
+        return current;
+    }, {})
+}
