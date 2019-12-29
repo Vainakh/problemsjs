859f2a9b172acc92a889ac44d2e7b0391dc0c4c2
Adlan <adlanyandarbiev@gmail.com>
2019-12-29 15:33:24 -0800

    Add test cases

--- __134_ keyCount.js
+++ __134_ keyCount.js
@@ -12,3 +12,16 @@ function keyCount(array){
         return current;
     }, {})
 }
+
+keyCount([
+  { name: "Elie", catOwner: false },
+  { name: "Moxie", isCat: true }
+]) // { name: 2, catOwner: 1, isCat: 1}
+
+keyCount([
+  { age: 1, eyeColor: "blue" },
+  { age: 3, eyeColor: "brown" },
+  { age: 7, inSchool: true }
+]) // { age: 3, eyeColor: 2, inSchool: 1 }
+
+Try to solve this using reduce.
