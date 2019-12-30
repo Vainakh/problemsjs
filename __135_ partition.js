d1d302b9563494382b55accd76a8cbfe0af7a8f9
Adlan <adlanyandarbiev@gmail.com>
2019-12-30 09:26:06 -0800

    Numerate and add techers instructions

---
+++ __135_ partition.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
+
+Examples:
