b2df7681693111f9d5d789e10ec00a1108fa9dfa
Adlan <adlanyandarbiev@gmail.com>
2019-12-30 09:27:16 -0800

    Finish functions body

--- __135_ partition.js
+++ __135_ partition.js
@@ -2,3 +2,24 @@ Instructions from your teacher:
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
 
 Examples:
+
+function partition(array, callback){
+
+    let truearray = [];
+    let falsearray = [];
+
+    for (let i = 0; i < array.length; i += 1) {
+        if(callback(array[i])){
+            truearray.push(array[i])
+        } else {
+            falsearray.push(array[i])
+        }
+    }
+
+    let newarray = [truearray, falsearray];
+    return newarray;
+}
+
+function isEven(val) {
+    return val % 2 === 0;
+}
