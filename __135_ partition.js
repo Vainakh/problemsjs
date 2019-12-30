2a6307d908e9c175778f739307540a890fbee254
Adlan <adlanyandarbiev@gmail.com>
2019-12-30 09:28:08 -0800

    Add functions and add test cases

--- __135_ partition.js
+++ __135_ partition.js
@@ -23,3 +23,17 @@ function partition(array, callback){
 function isEven(val) {
     return val % 2 === 0;
 }
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(partition(arr, isEven));
+// [[2,4,6,8], [1,3,5,7]];
+
+function isLongerThanThreeCharacters(val) {
+    return val.length > 3;
+}
+
+var names = ['Elie', 'Colt', 'Tim', 'Matt'];
+
+console.log(partition(names, isLongerThanThreeCharacters));
+// [['Elie', 'Colt', 'Matt'], ['Tim']]
