a3f1e2478d958f03343d45794089a3beabc0487b
Adlan <adlanyandarbiev@gmail.com>
2020-01-01 11:22:15 -0800

    Add test cases

--- __!38_mergeSortedArrays.js
+++ __!38_mergeSortedArrays.js
@@ -19,3 +19,29 @@ function mergeSortedArrays(array1, array2) {
     }
     return newarray;
 }
+
+var arr1 = [1, 3, 4, 5];
+var arr2 = [2, 4, 6, 8];
+console.log(mergeSortedArrays(arr1, arr2)); // [1,2,3,4,4,5,6,8]
+
+
+arr1 // [1,3,4,5];
+arr2 // [2,4,6,8];
+
+
+var arr3 = [-2, -1, 0, 4, 5, 6];
+var arr4 = [-3, -2, -1, 2, 3, 5, 7, 8];
+
+
+console.log(mergeSortedArrays(arr3, arr4)); // [-3,-2,-2,-1,-1,0,2,3,4,5,5,6,7,8]
+
+
+arr3 // [-2,-1,0,4,5,6];
+arr4 // [-3,-2,-1,2,3,5,7,8];
+
+
+var arr5 = [3, 4, 5]
+var arr6 = [1, 2]
+
+
+console.log(mergeSortedArrays(arr5, arr6)); // [1,2,3,4,5]
