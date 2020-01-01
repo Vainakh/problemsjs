3a71da86bc9df00d1ae00b8ea1e1a02e30334102
Adlan <adlanyandarbiev@gmail.com>
2019-12-31 19:25:12 -0800

    Numerate and add teachers instructions

---
+++ __136_swap.js
@@ -0,0 +1,4 @@
+nstructions from your teacher:
+Write a function called swap, which accepts an array and two numbers. The function should return the array with the values at each number swapped. The function should not create a new array. You can assume that each number will be within the range of 0 and the length of the array.
+
+Examples:
