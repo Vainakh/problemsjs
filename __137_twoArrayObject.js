5982864edce5ddd99cf7499cd788865483f8fc39
Adlan <adlanyandarbiev@gmail.com>
2019-12-31 19:27:53 -0800

    Finish functions body

--- __137_twoArrayObject.js
+++ __137_twoArrayObject.js
@@ -4,3 +4,24 @@ Write a function called twoArrayObject which accepts to arrays of varying length
 If there are not enough values, the rest of keys should have a value of null.
 
 If there not enough keys, just ignore the rest of values.
+
+function twoArrayObject(array1, array2) {
+
+    let newobject = {};
+
+
+
+    for (let i = 0; i < array1.length; i += 1)  {
+        newobject[array1[i]] = null;
+    }
+
+    for (let i = 0; i < array1.length; i += 1) {
+    if (i < array2.length) {
+        newobject[array1[i]] = array2[i];
+    } else {
+        continue;
+    }
+
+    }
+    return newobject;
+}
