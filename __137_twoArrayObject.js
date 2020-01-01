1a23435654641b02e6ed73870836af96266a98d4
Adlan <adlanyandarbiev@gmail.com>
2019-12-31 19:28:21 -0800

    Add test cases

--- __137_twoArrayObject.js
+++ __137_twoArrayObject.js
@@ -25,3 +25,7 @@ function twoArrayObject(array1, array2) {
     }
     return newobject;
 }
+
+console.log(twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3])); // {'a': 1, 'b': 2, 'c': 3, 'd': null}
+console.log(twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4])); // {'a': 1, 'b': 2, 'c': 3}
+console.log(twoArrayObject(['x', 'y', 'z'], [1, 2])); // {'x': 1, 'y': 2, 'z': nu
