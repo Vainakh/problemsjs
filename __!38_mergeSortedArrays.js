d8849b6c31ef27adfc4c238250eab00e3e7e5e0a
Adlan <adlanyandarbiev@gmail.com>
2020-01-01 11:20:43 -0800

    Numerate and add instructions from the teacher

---
+++ __!38_mergeSortedArrays.js
@@ -0,0 +1,4 @@
+Instructions from your teacher:
+Given two sorted arrays, write a function called mergeSortedArrays which accepts two arrays and returns a new array with both of the values from each array sorted.
+
+As a bonus, try to implement this without using the built-in sort method.
