7d9e0798c03303cfcf28bcfa580b330a9457b086
Adlan <adlanyandarbiev@gmail.com>
2019-12-31 19:27:18 -0800

    Numerate and add test cases

---
+++ __137_twoArrayObject.js
@@ -0,0 +1,6 @@
+Instructions from your teacher:
+Write a function called twoArrayObject which accepts to arrays of varying lengths.The first array consists of keys and the second one consists of values. Your function should return an object created from the keys and values.
+
+If there are not enough values, the rest of keys should have a value of null.
+
+If there not enough keys, just ignore the rest of values.
