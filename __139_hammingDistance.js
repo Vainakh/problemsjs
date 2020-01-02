d8990db7a7ab0e799784d9bbcb5656d026fed516
Adlan <adlanyandarbiev@gmail.com>
2020-01-02 04:05:27 -0800

    Finish functions body

--- __139_hammingDistance.js
+++ __139_hammingDistance.js
@@ -9,3 +9,22 @@ This function takes in two strings of equal length, and calculates the distance 
 If the inputs do not have the same length, the function should return "Input strings must have the same length."
 
 Examples
+
+function hammingDistance(string_1, string_2){
+    let string1 = string_1.toLowerCase();
+    let string2 = string_2.toLowerCase();
+    let difference = 0;
+
+    if(string1.length !== string2.length) {
+        return "Input strings must have the same length.";
+    }
+
+    for (let i = 0; i < string1.length; i += 1) {
+        for (let j = 0; j < string2.length; j += 1) {
+            if(string1[i] !== string2[j] && i === j) {
+                difference += 1;
+            }
+        }
+    }
+    return difference;
+}
